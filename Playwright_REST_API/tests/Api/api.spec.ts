6a48ac9624ab2c337866d73a8a9f01676126c4fc
Mass Frat <148748075+MassFrat@users.noreply.github.com>
2026-06-25 17:07:12 +0800

    Update api.spec.ts

--- Playwright_REST_API/tests/Api/api.spec.ts
+++ Playwright_REST_API/tests/Api/api.spec.ts
@@ -1,14 +1,13 @@
-import { test, expect } from '@playwright/test'
+import { test, expect } from '@playwright/test';
 
 test.describe.parallel('API Testing', () => {
-  const baseUrl = 'https://reqres.in/api'
+  const baseUrl = 'https://reqres.in/api';
 
   test('Simple API Test - Assert Response Status', async ({ request }) => {
-    const response = await request.get(`${baseUrl}/users/3`)
-    expect(response.status()).toBe(200)
-
-    const responseBody = JSON.parse(await response.text())
-  })
+    const response = await request.get(`${baseUrl}/users/2`);                 
+    expect(response.status()).toBe(200);
+    const responseBody = await response.json();
+  });
 
   test('Simple API Test - Assert Invalid Endpoint', async ({ request }) => {
     const response = await request.get(`${baseUrl}/users/non-existing-endpoint`)
